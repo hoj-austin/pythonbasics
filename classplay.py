eac0a8bdefb65033952d34a9b161fc62e1b7b4a6
hoj <joh.l.austin@gmail.com>
2020-05-04 10:35:53 +1000

    class instances

--- classplay.py
+++ classplay.py
@@ -19,4 +19,32 @@ class Aircraft: # because planes and helicopters are awesome
         else: 
             print('The aircraft type is unknown')
 
-    # def maintenance
+    def maintenance(self):
+        strHours = str(self.hours)
+        digits = len(strHours)
+        tens = strHours[digits-2:]
+        hrsRemain = 100 - int(tens)
+        print("Hours until 100 hourly maintenance interval: ", hrsRemain)
+
+
+# aeroplane Foxtrot X-ray Kilo 
+planeFXK = Aircraft('fixed', 2113, 180, 5)  # class instance = object
+print('Airpsace: ', planeFXK.airspace) # calling class variable
+print('Wing: ', planeFXK.wingType) # calling object attributes
+print('Total Time in Service: ', planeFXK.hours)
+print('Passengers: ', planeFXK.passengers)
+planeFXK.aircraftType() # calling object method
+planeFXK.maintenance()
+
+
+# helicopter Hotel Yankee Charlie: 
+heliHYC = Aircraft('rotary', 120, 130, 3) # class instance = object
+print('Airpspace: ', heliHYC.airspace) # calling class variable
+print('Wing: ', heliHYC.wingType) # calling object attributes
+print('Total Time In Service: ', heliHYC.hours)
+print('Max passengers: ', heliHYC.passengers)
+heliHYC.aircraftType() # calling object method
+heliHYC.maintenance()
+
+
+
